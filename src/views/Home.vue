a841a8c824a94cdcba6d71db70af7fb07c9faaad
Mohid Kazi <mohidkazix@gmail.com>
2021-04-25 20:50:53 +0530

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Home
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {},
+  data() {
+    return {
+      isLoading: true,
+      summaryDate: '',
+      summarymessage: '',
+      summaryID: '',
+      summaryGlobal: {},
+      status: {},
+      countries: [],
+      loadingIcon: require('../assets/spin.gif'),
+    }
+  },
+  methods: {
+    async fetchCovidSummary() {
+      const response = await fetch('https://api.covid19api.com/summary');
+      const data = await response.json();
+      return data;
+    },
+  },
+  async created() {
+    const covidSummary = await this.fetchCovidSummary();
+    console.log(covidSummary)
+  },
 }
 </script>
